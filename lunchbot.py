0bead68f8ff2445f099593d0c1045b6a89365c6f
cycleseven <unvexer@gmail.com>
2018-04-07 21:14:40 +0100

    Add API endpoint to handle Slack events (to test Slack API -> API Gateway -> Lambda -> Dynamo integration)

---
+++ lunchbot.py
@@ -0,0 +1,42 @@
+import boto3
+import json
+import os
+import uuid
+
+dynamodb = boto3.client("dynamodb")
+
+
+def on_slack_event(event, context):
+    try:
+        body = json.loads(event["body"])
+        payload = body["event"]
+    except KeyError:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({
+                "message": "Malformed event in request body."
+            })
+        }
+
+    if payload["text"].lower() == "yes":
+        print("Affirmative response detected using complex deep neural net algorithm.")
+        dynamo_response = dynamodb.put_item(
+            TableName=os.environ['DYNAMODB_TABLE'],
+            Item={
+                "id": {
+                    "S": str(uuid.uuid4())
+                },
+                "user": {
+                    "S": payload["user"]
+                },
+                "didBringLunch": {
+                    "BOOL": True
+                }
+            }
+        )
+        print(dynamo_response)
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({"cool": "stuff"})
+    }
