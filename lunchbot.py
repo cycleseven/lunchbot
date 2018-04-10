66ea85687ce04dbcb82b73d309b774c2535be7c4
cycleseven <unvexer@gmail.com>
2018-04-10 13:19:04 +0100

    React to "yes" with a thumbsup emoji

--- lunchbot.py
+++ lunchbot.py
@@ -2,7 +2,10 @@ import boto3
 import json
 import os
 import uuid
+from slackclient import SlackClient
 
+slack_token = os.environ["SLACK_API_TOKEN"]
+sc = SlackClient(slack_token)
 dynamodb = boto3.client("dynamodb")
 
 
@@ -10,12 +13,19 @@ def on_slack_event(event, context):
     try:
         body = json.loads(event["body"])
         payload = body["event"]
+        print(body)
     except KeyError:
         return {
             "statusCode": 400,
             "body": json.dumps({
                 "message": "Malformed event in request body."
             })
+        }
+
+    if payload["type"] != "message":
+        return {
+            "statusCode": 200,
+            "body": "Ignoring non-message event."
         }
 
     if payload["text"].lower() == "yes":
@@ -35,6 +45,12 @@ def on_slack_event(event, context):
             }
         )
         print(dynamo_response)
+        sc.api_call(
+            "reactions.add",
+            channel=payload["channel"],
+            name="thumbsup",
+            timestamp=payload["ts"]
+        )
 
     return {
         "statusCode": 200,
