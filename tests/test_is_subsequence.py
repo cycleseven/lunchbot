939bc79e15fee8f956437788ecf759123cb82602
cycleseven <unvexer@gmail.com>
2018-04-28 14:16:06 +0100

    Fix edge case of is_subsequence

--- tests/test_is_subsequence.py
+++ tests/test_is_subsequence.py
@@ -1,25 +1,37 @@
 from utils import is_subsequence
 
 
-def test_should_return_true_if_short_list_is_subsequence_of_long_list():
+def test_should_return_true_for_subsequence_at_end_of_long_list():
     assert is_subsequence(["y", "e", "s"], ["hello", "and", "y", "e", "s"])
+
+
+def test_should_return_true_for_subsequence_in_middle_of_long_list():
+    assert is_subsequence(["y", "e", "s"], ["actually", "y", "e", "s", "haha"])
+
+
+def test_should_return_true_for_subsequence_at_start_of_long_list():
+    assert is_subsequence(["y", "e", "s"], ["y", "e", "s", "haha"])
 
 
 def test_should_return_true_if_lists_are_equal():
     assert is_subsequence(["y", "e", "s"], ["y", "e", "s"])
 
 
-def test_should_return_false_if_first_argument_is_actually_the_longer_list():
-    assert not is_subsequence(["l", "o", "n", "g"], ["l", "o"])
-
-
 def test_should_return_true_if_both_lists_are_empty():
     assert not is_subsequence([], [])
 
 
-def test_should_handle_subsequence_in_middle_of_list():
-    assert not is_subsequence(["actually", "y", "e", "s", "haha"], ["y", "e", "s"])
+def test_should_return_false_if_short_list_is_not_subsequence_of_long_list():
+    assert not is_subsequence(['l', 'o', 'l'], ['d', 'i', 'f', 'f', 'e', 'r', 'e', 'n', 't'])
 
 
-def test_should_handle_subsequence_at_start_of_list():
-    assert not is_subsequence(["y", "e", "s", "haha"], ["y", "e", "s"])
+def test_should_return_false_if_first_argument_is_actually_the_longer_list():
+    assert not is_subsequence(["l", "o", "n", "g"], ["l", "o"])
+
+
+def test_should_return_false_for_partial_match_at_end_of_long_list():
+    assert not is_subsequence(['a', 'b', 'c'], ['1', '2', 'a', 'b'])
+
+
+def test_should_return_false_for_partial_match_in_middle_of_long_list():
+    assert not is_subsequence(['a', 'b'], ['1', 'a', '3'])
--- utils.py
+++ utils.py
@@ -16,6 +16,12 @@ def is_subsequence(short_list, long_list):
             if len(long_list) - j == 0:
                 break
 
+            # In this case, we've reached the end of the long list and still only have
+            # a partial match. So we're not going to get one.
+            if i + j == len(long_list):
+                looks_good = False
+                break
+
             if item_from_short_list != long_list[i + j]:
                 looks_good = False
                 break
