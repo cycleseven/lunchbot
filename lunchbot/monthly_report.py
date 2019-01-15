85736dfc19e8498c138d3a6ec7977cec1be91a29
cycleseven <unvexer@gmail.com>
2019-01-15 23:00:10 +0000

    Add some words of encouragement: untested so far

--- lunchbot/monthly_report.py
+++ lunchbot/monthly_report.py
@@ -74,7 +74,93 @@ def get_winners(stats):
     return [stat["name"] for stat in stats if stat["good_days"] == max_good_days]
 
 
+def get_words_of_encouragement(ratio):
+    """Return an English sentence summarising how good the ratio is"""
+
+    if 0 <= ratio <= 0.2:
+        return random.choice([
+            "there is nowhere to go but up",
+            "that is not that good sorry",
+            "clean your room",
+            "there is always next month",
+            "lol",
+            "to be honest katsu curry is really nice so fair play",
+        ])
+    elif 0.2 < ratio <= 0.4:
+        return random.choice([
+            "it is an ok ratio i think but i think you can do even better next time but well done though",
+            "cool",
+            "not bad",
+            "if you don't like having money then this is a pretty good ratio",
+            "with more dedication i'm sure you can do better next time but well done",
+            "hm"
+        ])
+    elif 0.4 < ratio <= 0.6:
+        return random.choice([
+            "it is a good ratio well done",
+            "not bad at all",
+            "it is a nice one",
+            "this is very respectable congratulations",
+            "you are good",
+            "yes",
+        ])
+    elif 0.6 < ratio <= 0.8:
+        return random.choice([
+            "that really is great",
+            "gawn yersel",
+            "that is a fantastic ratio, enjoy your money",
+            "this is how you do it everyone",
+            "WOAH!!! good one",
+            "a picture of excellence",
+            "perfect 5/7"
+        ])
+    elif 0.8 < ratio <= 1:
+        return random.choice([
+            "a tremendous display of dedication to the art of preparation",
+            "a true hero",
+            "wow that is an inspiring ratio",
+            "well well well !!! look at what we have here, that is really incredible. well done",
+            "i never thought i would see this in my lifetime wow",
+            "now you are just showing off",
+        ])
+    else:
+        raise ValueError(f"Unhandled ratio {ratio}")
+
+
+def get_concrete_example_of_the_value_of_saving(estimated_money_saved):
+    # Either give the estimated money saved as a % of a huge goal, or
+    # say how many small things could be purchased
+    if random.choice([True, False]):
+        # The expensive goals are in this branch
+        goal = random.choice([
+            {"name": "a housing deposit in london", "value_gbp": 80000},
+            {"name": "a Tesla Model S™", "value_gbp": 73500},
+            {"name": "an average second-hand yacht", "value_gbp": 6528984},
+            {
+                "name": "a small loan of a million dollars for your children (according to exchange rate at time of writing)",
+                "value_gbp": 777200
+            },
+        ])
+
+        percentage_of_goal_achieved = (estimated_money_saved / goal["value"]) * 100
+        return f"{percentage_of_goal_achieved}% of {goal['name']}"
+    else:
+        # The cheap goals are in this branch
+        goal = random.choice([
+            {"name": "bananas from a convenience supermarket", "value_gbp": 0.25},
+            {"name": "bananas from a big supermarket", "value_gbp": 0.13},
+            {"name": "items from a pound shop", "value_gbp": 1},
+            {"name": "plastic coat hangers", "value_gbp": 0.26},
+            {"name": "balls of 200 multicolored rubber bands", "value_gbp": 5},
+        ])
+
+        number_of_goals_achieved = estimated_money_saved / goal["value"]
+        return f"{number_of_goals_achieved} {goal['name']}"
+
+
 def summarise_stat(stat):
+    """Return an English paragraph summarising the monthly stat for a user"""
+
     pet_names = [
         "sweet pea",
         "human",
@@ -93,60 +179,12 @@ def summarise_stat(stat):
 
     ratio = stat["good_days"] / stat["total_days"]
 
-    if 0 <= ratio <= 0.2:
-        words_of_encouragement = random.choice([
-            "there is nowhere to go but up",
-            "that is not that good sorry",
-            "clean your room",
-            "there is always next month",
-            "lol",
-            "to be honest katsu curry is really nice so fair play",
-        ])
-    elif 0.2 < ratio <= 0.4:
-        words_of_encouragement = random.choice([
-            "it is an ok ratio i think but i think you can do even better next time but well done though",
-            "cool",
-            "not bad",
-            "if you don't like having money then this is a pretty good ratio",
-            "with more dedication i'm sure you can do better next time but well done",
-            "hm"
-        ])
-    elif 0.4 < ratio <= 0.6:
-        words_of_encouragement = random.choice([
-            "it is a good ratio well done",
-            "not bad at all",
-            "it is a nice one",
-            "this is very respectable congratulations",
-            "you are good",
-            "yes",
-        ])
-    elif 0.6 < ratio <= 0.8:
-        words_of_encouragement = random.choice([
-            "that really is great",
-            "gawn yersel",
-            "that is a fantastic ratio, enjoy your money",
-            "this is how you do it everyone",
-            "WOAH!!! good one",
-            "a picture of excellence",
-            "perfect 5/7"
-        ])
-    elif 0.8 < ratio <= 1:
-        words_of_encouragement = random.choice([
-            "a tremendous display of dedication to the art of preparation",
-            "a true hero",
-            "wow that is an inspiring ratio",
-            "well well well !!! look at what we have here, that is really incredible. well done",
-            "i never thought i would see this in my lifetime wow",
-            "now you are just showing off",
-        ])
-    else:
-        raise ValueError(f"Unhandled ratio {ratio}")
-
     return (
         f'{stat["name"]} my {random.choice(pet_names)}\n'
         f'{stat["good_days"]}/{stat["total_days"]}\n'
-        f'{words_of_encouragement}\n'
+        f'{get_words_of_encouragement(ratio)}\n'
         f'you have saved an estimated £{stat["estimated_money_saved"]} this month\n'
+        f'with that money you could buy {get_concrete_example_of_the_value_of_saving(stat["estimated_money_saved"])}'
     )
 
 
@@ -158,6 +196,8 @@ def summarise_winners(winners):
 
 
 def summarise_results(stats):
+    """Return the text for the monthly report"""
+
     stat_summaries = "\n".join([summarise_stat(stat) for stat in stats])
     winners = get_winners(stats)
     greeting = random.choice([
